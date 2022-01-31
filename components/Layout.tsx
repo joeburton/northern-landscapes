8eab94a890a8cc27ceff13bfe2cbd032972d39e4
joe burton <joeburton@gmail.com>
2022-01-31 12:06:07 +0000

    google verification

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,33 +1,37 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode } from "react";
 
-import Head from 'next/head';
-import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container';
-import Header from './Header';
-import Footer from './Footer';
+import Head from "next/head";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import Header from "./Header";
+import Footer from "./Footer";
 
 type Props = {
   children?: ReactNode;
   title?: string;
 };
 
-const Layout = ({ children, title = 'Northern Landscapes' }: Props) => (
+const Layout = ({ children, title = "Northern Landscapes" }: Props) => (
   <div>
     <Head>
       <title>{title}</title>
-      <meta charSet='utf-8' />
-      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      <link
-        rel='stylesheet'
-        href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta
+        name="google-site-verification"
+        content="2TVck6TXQvFOku1IHundCfovdIcHQfM34IHTCWz83jo"
       />
       <link
-        rel='stylesheet'
-        href='https://fonts.googleapis.com/icon?family=Material+Icons'
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
       />
     </Head>
-    <Container component='section' maxWidth='md'>
-      <Grid container spacing={3} alignItems='stretch'>
+    <Container component="section" maxWidth="md">
+      <Grid container spacing={3} alignItems="stretch">
         <Grid item xs={12}>
           <Header />
         </Grid>
